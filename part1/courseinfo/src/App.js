a59c3fa09dd5d6a93894bdcdca08d1d1b1379454
Hoang Ho <hoangkplog@hoangkplog.localdomain>
2022-04-24 13:29:43 +0200

    1.3 to 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,30 +1,26 @@
 const App = () => {
-	const course = "Half Stack application development";
-	// const part1 = "Fundamentals of React";
-	// const exercises1 = 10;
-	// const part2 = "Using props to pass data";
-	// const exercises2 = 7;
-	// const part3 = "State of a component";
-	// const exercises3 = 14;
-	let parts = [
-		{
-			name: "Fundamentals of React",
-			exercises: 10,
-		},
-		{
-			name: "Using props to pass data",
-			exercises: 7,
-		},
-		{
-			name: "State of a component",
-			exercises: 14,
-		},
-	];
+	const course = {
+		name: "Half Stack application development",
+		parts: [
+			{
+				name: "Fundamentals of React",
+				exercises: 10,
+			},
+			{
+				name: "Using props to pass data",
+				exercises: 7,
+			},
+			{
+				name: "State of a component",
+				exercises: 14,
+			},
+		],
+	};
 	return (
 		<div>
-			<Header course={course} />
-			<Content parts={parts} />
-			<Total parts={parts} />
+			<Header course={course.name} />
+			<Content parts={course.parts} />
+			<Total parts={course.parts} />
 		</div>
 	);
 };
